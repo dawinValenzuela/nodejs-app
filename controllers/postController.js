00ca3e68085377c235d654b5c9541f252ea3df0a
Dawin Valenzuela <dawin.valenzuela@gmail.com>
2019-11-26 00:31:47 -0500

    delete post function

--- controllers/postController.js
+++ controllers/postController.js
@@ -78,18 +78,17 @@ exports.updatePost = (req, res) => {
   });
 };
 
-exports.deletePost = (req, res) => {
-  const children = posts.data.children;
-  const post = children.find(el => el.data.id === req.params.id);
-
-  if (!post) {
-    res.status(404).json({
-      status: "fail",
-      message: "Invalid Id"
-    });
-  }
-
-  res.status(204).json({
-    status: "success"
-  });
+exports.deletePost = async (req, res) => {
+    try {
+        const post = await Post.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            status: 'success',
+            data: null
+          });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+          });
+    }
 };
